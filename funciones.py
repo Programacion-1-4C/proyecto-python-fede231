4c81fd291da73204e4465bb43eb107e4411d513c
federico <federicoboiko9@gmail.com>
2022-08-17 10:31:01 -0300

    llegue a las 50 palabras, cree algunas funciones y hice algunas cosas basicas en el main

--- funciones.py
+++ funciones.py
@@ -1,16 +1,73 @@
-palabras_wordle = {1:'huevo'
-                   2:'pasto'
-                   3:'queso'
-                   4:'fuego'
-                   5:'salmon'
-                   6:'chayle'
-                   7:'hueso'
-                   8:'carne'
-                   9:'papas'
-                   10:'rimar'
-                   11:'salir
-                   12:'comer'
-                   13:'pasar'
-                   14:'tocar'
-                   15:'salvay'
-                   16:'partir'}
+import random
+from random import randint
+palabras_wordle = {1:'huevo',
+                   2:'pasto',
+                   3:'queso',
+                   4:'fuego',
+                   5:'salmon',
+                   6:'chayle',
+                   7:'hueso',
+                   8:'carne',
+                   9:'dia',
+                   10:'rimar',
+                   11:'salir',
+                   12:'especial',
+                   13:'pasar',
+                   14:'tocar',
+                   15:'salvay',
+                   16:'partir',
+                   17.:'mouse',
+                   18.:'cable',
+                   19.:'arbol',
+                   20.:'cafe',
+                   21.:'calor',
+                   22.:'teclas',
+                   23.:'bañar',
+                   24.:'cantar',
+                   25.:'amar',
+                   26.:'brazo',
+                   27.:'pierna',
+                   28.:'pastas',
+                   29.:'querer',
+                   30.:'papas',
+                   31.:'comer',
+                   32.:'hacer',
+                   33.:'bien',
+                   34.:'club',
+                   35.:'pelea',
+                   36.:'extrañar',
+                   37.:'dados',
+                   38.:'suerte',
+                   39.:'perdon',
+                   40.:'esperar',
+                   41.:'polea',
+                   42.:'olvidar',
+                   43.:'pensar',
+                   44.:'mejorar',
+                   45.:'monton',
+                   46.:'miniom',
+                   47.:'moscas',
+                   48.:'tiempo',
+                   49.:'mentir',
+                   50.:'arreglar',}
+
+
+
+
+def elegir_palabra (palabras_wordle):
+    palabra_random = random.randint(1,50)
+    palabra = palabras_wordle(palabra_random)
+    print (palabra)
+
+def acertar_palabra (intento):
+    if intento == palabras_wordle[]:
+        print('ganaste')
+    else:
+        pass
+
+def contar_letras:
+    count(elegir_palabra())
+
+def letras_repetidas():
+
+def letras_no_usadas():
--- main.py
+++ main.py
@@ -1,3 +1,31 @@
+import random
+from funciones import palabras_wordle
+from funciones import elegir_palabra
+from funciones import letras_repetidas
+from funciones import letras_no_usadas
+from funciones import contar_letras
+from funciones import acertar_palabra
+
+
 print('que vas a hacer?')
-decision = input('1. Jugar'
-                 '2. Agregar palabras')
+intentos = 0
+while True:
+    decision = input('1. Jugar'
+                     '2. Agregar palabras'
+                     '3.salir')
+    if decision == '1':
+        elegir_palabra(palabras_wordle)
+        intento = input('dame una letra')
+        intento = intentos+1
+        if intentos > 5:
+            break
+
+    elif decision == '2':
+        palabra = input('que palabra vas a agregar')
+        palabras_wordle.append(palabra)
+
+
+    elif decision == '3':
+
+    else:
+        break
