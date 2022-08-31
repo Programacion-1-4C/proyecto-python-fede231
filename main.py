b1704a19a4ea5c7a2e038b51746cf986b49fe253
federico <federicoboiko9@gmail.com>
2022-08-31 08:01:17 -0300

    cambie todo y agregue una bocha de palabras

--- main.py
+++ main.py
@@ -1,31 +1,50 @@
 import random
-from funciones import palabras_wordle
-from funciones import elegir_palabra
-from funciones import letras_repetidas
-from funciones import letras_no_usadas
-from funciones import contar_letras
-from funciones import acertar_palabra
+from funciones import *
+win = False
+palabra_random = random.choice(palabras_wordle)
+colors = {
+    'green': '\033[92m',
+    'yellow': '\033[93m',
+    'red': '\033[91m',
+    'ENDC': '\033[0m',
+}
+def color_letter(letter, color):
+    return colors[color] + letter + colors['ENDC']
+decision = input('1. Jugar\n')
+if decision == '1':
+    board = []
+    for i in range(6):
+        board.append(['_' for l in range(6)])
+
+    game_loop_counter = 0
+    while (not win) and (game_loop_counter < len(palabra_random)):
+        text = input ('')
+        while len (text) != len(palabra_random):
+            print(f'la palabra debe tener {len(palabra_random)} de caracteres')
+            text = input('')
 
 
-print('que vas a hacer?')
-intentos = 0
-while True:
-    decision = input('1. Jugar'
-                     '2. Agregar palabras'
-                     '3.salir')
-    if decision == '1':
-        elegir_palabra(palabras_wordle)
-        intento = input('dame una letra')
-        intento = intentos+1
-        if intentos > 5:
-            break
+        if palabra_random == text:
+            board[game_loop_counter] = [l for l in text]
+            win = True
+        else:
+            test_line = []
+            for j in range(len(text)):
+                if text[j] == palabra_random[j]:
+                    test_line.append(text[j])
+                elif text[j] in palabra_random:
+                    test_line.append(color_letter(text[j],'yellow'))
 
-    elif decision == '2':
-        palabra = input('que palabra vas a agregar')
-        palabras_wordle.append(palabra)
+                else:
+                    test_line.append(color_letter(text[j],'red'))
+            board[game_loop_counter] = test_line
+        for i in range(6):
+            print(" ".join(board[i]))
+
+        game_loop_counter += 1
 
 
-    elif decision == '3':
 
-    else:
-        break
+
+
+
